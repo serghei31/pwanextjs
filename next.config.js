69d206c69d71eb3d7e14ec9b80a472ba7f340fe7
Serghei Covali <sergheic.89@gmail.com>
2023-12-01 12:19:25 +0200

    Delete old method

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // ... other options you like
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
