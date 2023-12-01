ad9af571006d3e71309784ba5de63aae3dcf67da
Serghei Covali <sergheic.89@gmail.com>
2023-12-01 14:53:09 +0200

    Use ducanh2912 next-pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa");
+// const withPWA = require("@ducanh2912/next-pwa");
 
-const nextConfig = {
-  ...withPWA.default({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  }),
-};
+// const nextConfig = {
+//   ...withPWA.default({
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   }),
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
